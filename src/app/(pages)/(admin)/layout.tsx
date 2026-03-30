aa4b779473d84f74c4181d18098b6d45051678fc
Nipun <nipunbhanuka12@gmail.com>
2026-03-30 22:47:45 +0530

    change folder path of authguard

--- src/app/(pages)/(admin)/layout.tsx
+++ src/app/(pages)/(admin)/layout.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import AuthGuard from "@/src/components/AuthGuard";
+import AuthGuard from "@/src/app/(pages)/auth/AuthGuard";
 
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
   return <AuthGuard allowedRoles={['ADMIN']}>{children}</AuthGuard>;
--- src/app/(pages)/(users)/layout.tsx
+++ src/app/(pages)/(users)/layout.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import AuthGuard from "@/src/components/AuthGuard";
+import AuthGuard from "@/src/app/(pages)/auth/AuthGuard";
 
 export default function UserLayout({ children }: { children: React.ReactNode }) {
   return <AuthGuard allowedRoles={['USER']}>{children}</AuthGuard>;
